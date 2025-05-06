a5bf37f3cb95c6b3bce1375da4f966df7b3903bd
Don <tiago06cc@gmail.com>
2025-05-06 15:05:10 +0200

    fixed Projcets cards, fixed typos, fixed Images

--- portfolio/components/ui/Pin.tsx
+++ portfolio/components/ui/Pin.tsx
@@ -159,7 +159,7 @@ export const PinPerspective = ({
             ></motion.div>
           </>
         </div>
-å
+
         <>
           <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40 blur-[2px]" />
           <motion.div className="absolute right-1/2 bottom-1/2 bg-gradient-to-b from-transparent to-cyan-500 translate-y-[14px] w-px h-20 group-hover/pin:h-40  " />
